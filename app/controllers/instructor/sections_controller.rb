6d2d7d6255116c451d3a119c95158af36300648f
Jeffrey Patajo <jpatajo@gmail.com>
2017-04-16 08:18:59 +0000

    Created new lesson form modal

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -17,16 +17,16 @@ class Instructor::SectionsController < ApplicationController
   
   def require_authorized_for_current_course
     if current_course.user != current_user
-      render text: "Unauthorized", status: :unauthorized
+      render plain: "Unauthorized", status: :unauthorized
     end
   end
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      return render text: 'Unauthorized', status: :unauthorized
+      render plain: 'Unauthorized', status: :unauthorized
     end
   end
-  
+
   def current_section
     @current_section ||= Section.find(params[:id])
   end
